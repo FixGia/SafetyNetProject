ae146d43ab8d1d3d19dc808e1c87431dde092650
FixGia <fx.giracca@gmail.com>
2021-08-25 12:14:12 +0200

    create Project

---
+++ src/main/java/com/project/apisafetynet/ApiSafetyNetApplication.java
@@ -0,0 +1,13 @@
+package com.project.apisafetynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSafetyNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSafetyNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/apisafetynet/ApiSafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apisafetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSafetyNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
